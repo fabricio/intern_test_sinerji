9419e57dddbcb446883f577769f955bd24922819
Fabrício Soares <eu@fabriciosoares.com>
2025-05-25 16:32:41 -0300

    class funcionario

---
+++ src/Funcionario.java
@@ -0,0 +1,21 @@
+import java.time.LocalDate;
+
+public abstract class Funcionario {
+    protected String nome;
+    protected LocalDate dataContratacao;
+
+    public Funcionario(String nome, LocalDate dataContratacao) {
+        this.nome = nome;
+        this.dataContratacao = dataContratacao;
+    }
+
+    public abstract double calcularSalario(int mes, int ano);
+    public abstract double calcularBeneficio(int mes, int ano);
+
+    public double calcularTotalRecebido(int mes, int ano) {
+        return calcularSalario(mes, ano) + calcularBeneficio(mes, ano);
+    }
+    public String getNome() {
+        return nome;
+    }
+}
